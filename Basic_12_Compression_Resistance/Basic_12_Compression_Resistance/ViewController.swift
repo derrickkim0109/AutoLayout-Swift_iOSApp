0c09234678f4d1fdd4d625571e379aed5a43eb55
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-19 16:26:11 +0900

    21.04.19_Compression_Resistancce

---
+++ Basic_12_Compression_Resistance/Basic_12_Compression_Resistance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_12_Compression_Resistance
+//
+//  Created by Derrick kim on 2021/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
