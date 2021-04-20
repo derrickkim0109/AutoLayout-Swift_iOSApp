d9ea00a9d9729661a6eb4c545fbd991f44025ecb
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-20 11:47:54 +0900

    21.04.20. Basic_21_AutoLayout_Code_Anchor_02

--- Basic_21_AutoLayout_Code_Anchor_01/Basic_21_AutoLayout_Code_Anchor_01/ViewController.swift
+++ Basic_21_AutoLayout_Code_Anchor_01/Basic_21_AutoLayout_Code_Anchor_01/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // ---------------------------------- // 
+        // Basic_21_AutoLayout_Code_Anchor_01 //
+        // ---------------------------------- //
+        
         
         // Button을 Main StoryBoard에 추가 하는 법
         let myButton = UIButton.init(type: .system)
@@ -29,8 +33,27 @@ class ViewController: UIViewController {
         myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         
         // 위에것 대신 하는 것.
-//        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -100).isActive = true
-        myButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -100).isActive = true
+//        myButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
+        
+        // ---------------------------------- //
+        // Basic_21_AutoLayout_Code_Anchor_02 //
+        // ---------------------------------- //
+        
+        
+        let myLabel = UILabel.init()
+        myLabel.text = "My Label"
+        
+        self.view.addSubview(myLabel)
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // SafeArea를 침범하지 않는 영역 내에서 라벨을 떨어뜨림.
+        
+        myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor  , constant: 20).isActive = true
+        myLabel.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor , constant: 20).isActive = true
+        
+        
+        
         
     }
 
