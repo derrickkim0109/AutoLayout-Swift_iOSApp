bdc32730d1335cc9474fcfd4ad71e64b99045fd0
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-20 11:16:54 +0900

    21.04.20. Basic_19_Image_Resolution

---
+++ Basic_19_Image_Resolution/Basic_19_Image_Resolution/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_19_Image_Resolution
+//
+//  Created by Derrick kim on 2021/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
